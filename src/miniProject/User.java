2615c9d126f7daf28ed2ab091953905b1f019df8
haegong <rlagorhd@gmail.com>
2024-04-23 16:15:47 +0900

    받아라

--- src/miniProject/User.java
+++ src/miniProject/User.java
@@ -4,5 +4,31 @@ package miniProject;
 public class User {
 	private String id; // 사용자 아이디
 	private int score; // 사용자 점수
-}
+	
 // 유저 get set 세팅하고 생성자 생성.
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	// 생성자 
+	
+	public User(String id, int score) {
+		super();
+		this.id = id;
+		this.score = score;
+	}
+	
+	
+	
+	
+}
